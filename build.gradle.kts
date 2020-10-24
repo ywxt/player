981b4f13eb431bb56a7b07c6d5bbb66abbbc6324
ywxt <ywxtcwh@qq.com>
2020-10-24 17:56:23 +0800

    初始化项目

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm") version "1.4.10"
+}
+
+group = "ywxt.pi"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
